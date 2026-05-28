22c958cc8a4da388bb1ffbf302ac4f80f14da34a
perkuliahanonline123-droid <perkuliahanonline123@gmail.com>
2026-05-28 12:06:01 +0700

    api.ts

--- presensi-&-jurnal-perkuliahan-staiis/src/services/api.ts
+++ presensi-&-jurnal-perkuliahan-staiis/src/services/api.ts
@@ -1,6 +1,32 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
- */
+const API_URL =
+  "https://script.google.com/macros/s/AKfycbyXHwbUiHQB1k0QmYDGIlt4T_WidAvqsGKcVjwJANE_BTSzej9kNl1MMWIxxcH4sk9jPw/exec";
 
-export { ApiClient, localDb } from '../db';
+export class ApiClient {
+
+  static async login(email: string, password: string) {
+
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        action: "login",
+        email,
+        password,
+      }),
+    });
+
+    return await response.json();
+  }
+
+  static async getUsers() {
+
+    const response = await fetch(
+      `${API_URL}?action=getUsers`
+    );
+
+    return await response.json();
+  }
+
+}
